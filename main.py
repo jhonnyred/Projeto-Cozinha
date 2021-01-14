bfed28621b66a6443ef93deb00723d5613c00168
jhonnyred <73783043+jhonnyred@users.noreply.github.com>
2021-01-13 21:13:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import json
+with open('estoque.txt') as dados:
+    json_data = json.load(dados)
+
 
 inicio = "1"
 while inicio == "1":
@@ -9,24 +13,41 @@ while inicio == "1":
         print("voce respondeu sim")
         procura = input("Qual ingrediente gostaria de modificar a pesagem?:  ")
 
-        if procura == "arroz" or "Arroz" or "ARROZ":
+        procura = procura.lower()
+
+        if procura == "arroz":
             arrozP = input("Peso do arroz em grama: ")
+            
             inicio = "1"
 
-        elif procura == "feijão" or "feijao" or "Feijão" or "Feijao" or "FEIJÃO" or "FEIJAO":
+        elif procura == "feijao":
             feijaoP = input("Peso do feijão em grama: ")
+            
             inicio = "1"
-
-        
+            
+       
 
     else:
         print("você respondeu não")
         acess = input("gostaria de acessar o estoque?:  ")
         if acess == "1":
-            estoque = {"arroz": arrozP, "feijão": feijaoP }
+            estoque = {"arroz": arrozP, "feijao": feijaoP }
             print(estoque)
+            
+            
+            
+            inicio = "1"
+        
+        else:
             inicio = "1"
 
+
+#j = json.loads('{"Nome": "Fulano", "Idade": "23"}')
+#j['Idade'] = '12'
+
+
+#with open('estoque.txt', 'a') as dados:
+#                dados.write(json.dumps(str(estoque)) + '\n')
 
 
 
